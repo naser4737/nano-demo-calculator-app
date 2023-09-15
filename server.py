c502759bc3413635b51cce9227acbcbf67125c37
naser4737 <nfornasir123@gmail.com>
2023-09-16 02:33:18 +0530

    Try1

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request , jsonify
+
 
 app = Flask(__name__)
 
@@ -9,11 +10,25 @@ def greeting():
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    try:
+        num = request.get_json()
+        first = num['first']
+        second = num['second']
+        result = first + second
+        return jsonify({'result': result}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    try:
+        num = request.get_json()
+        first = num['first']
+        second = num['second']
+        result = first - second
+        return jsonify({'result': result}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
